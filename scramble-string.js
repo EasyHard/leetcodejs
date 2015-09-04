eef8cccd8c5f115f3cc9ec46882d1e55ae546b21
easyhard <liuzhiyou.cs@gmail.com>
2015-09-05 00:51:59 +0800

    solve TLE by using [][]

--- scramble-string.js
+++ scramble-string.js
@@ -7,24 +7,33 @@ var DEBUG = process.env.DEBUG;
  */
 var isScramble = function(s1, s2) {
   var m = {};
+  function set(s, t, v) {
+    m[s] = m[s] || {};
+    m[s][t] = v;
+    return v;
+  }
+  function get(s, t) {
+    if (m[s] === undefined) return m[s];
+    else return m[s][t];
+  }
   function f(s, t) {
-    if (m[[s, t]] !== undefined) return m[s, t];
-    if (s === t) return m[[s, t]] = true;
+    var v;
+    if ((v = get(s, t)) !== undefined) return v;
+    if (s === t) return set(s, t, true);
     for (var i = 1; i < s.length; i++) {
       if (f(s.substr(0, i), t.substr(-i)) &&
           f(s.substr(i), t.substr(0, t.length-i)))
-        return m[[s,t]] = true;
+        return set(s,t,true);
       if (f(s.substr(0, i), t.substr(0, i)) &&
           f(s.substr(i), t.substr(i)))
-        return m[[s,t]] = true;
+        return set(s,t,true);
     }
-    return m[[s,t]] = false;
+    return set(s,t,false);
   }
   return f(s1, s2);
 };
 
 function test(f) {
-
   [
     ["great", "great"],
     ["great", "eatgr"],
@@ -32,6 +41,8 @@ function test(f) {
     ["great", "rgtae"],
     ["great", "rtgea"],
     ["dsanujiiqwfsysnfsrwbrfhhpqicbw", "dabbciwqphhfrwrsfnsysfwqiijuns"],
+    ["pcighfdjnbwfkohtklrecxnooxyipj", "npodkfchrfpxliocgtnykhxwjbojie"],
+    ["iydzdwbqbfixognqhbmimhwyhmdnrm", "nmbywdbnmmfybqqighdriizmxdhwho"],
   ].forEach(function (input) {
     console.log(f.apply(undefined, input));
   });
